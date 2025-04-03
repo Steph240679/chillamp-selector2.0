440141ae1b47b6ad03c5a11e77831e34614ea116
Steph240679 <stephane240679@gmail.com>
2025-04-03 21:44:49 +0200

    Create profils_sonores.py

---
+++ profils_sonores.py
@@ -0,0 +1,44 @@
+# profils_sonores.py
+
+profils_sonores = {
+    "growl": {
+        "tendance": {
+            "bass": +15,
+            "mid": +20,
+            "treble": -10,
+            "compression": "soft"
+        }
+    },
+    "claquant": {
+        "tendance": {
+            "bass": -10,
+            "mid": 0,
+            "treble": +20,
+            "compression": "strong"
+        }
+    },
+    "punch": {
+        "tendance": {
+            "bass": 0,
+            "mid": +15,
+            "treble": -5,
+            "compression": "medium"
+        }
+    },
+    "attaque": {
+        "tendance": {
+            "bass": -5,
+            "mid": +10,
+            "treble": +5,
+            "compression": "medium"
+        }
+    },
+    "rondeur": {
+        "tendance": {
+            "bass": +20,
+            "mid": -5,
+            "treble": -10,
+            "compression": "light"
+        }
+    }
+}
