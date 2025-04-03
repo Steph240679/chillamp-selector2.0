1b01a71902de50ee825f76801d6eaffacac88d8b
Steph240679 <stephane240679@gmail.com>
2025-04-03 21:43:16 +0200

    Create base_bassistes.py

---
+++ base_bassistes.py
@@ -0,0 +1,25 @@
+# Dossier : chillamp-selector2.0
+# Fichier : base_bassistes.py (réduit à 5 bassistes emblématiques)
+
+base_bassistes = {
+    "Pastorius Jaco": {
+        "caractere": "Son légendaire fretless, très chantant. growl",
+        "reglages_effets": {}
+    },
+    "Miller Marcus": {
+        "caractere": "Son claquant, slappé, très net et articulé. claquant",
+        "reglages_effets": {}
+    },
+    "Mendel Nate": {
+        "caractere": "Son solide et efficace pour le rock US. punch",
+        "reglages_effets": {}
+    },
+    "Vernerey Laurent": {
+        "caractere": "Son chaud, précis, grave bien tenu, très propre en studio live. attaque",
+        "reglages_effets": {}
+    },
+    "Jones John Paul": {
+        "caractere": "Son grave, précis et organique. rondeur",
+        "reglages_effets": {}
+    }
+}
