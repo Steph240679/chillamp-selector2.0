5bc2f23851b397ffe8aefa0d471eaa855f745065
Steph240679 <stephane240679@gmail.com>
2025-04-03 21:47:27 +0200

    Create amplis_basse_etendus.py

---
+++ amplis_basse_etendus.py
@@ -0,0 +1,46 @@
+# amplis_basse_etendus.py
+
+amplis_basse = {
+    "Ampeg SVT-3 Pro": {
+        "eq": ["Bass", "Midrange", "Treble"],
+        "neutral": {
+            "Bass": 50,
+            "Midrange": 50,
+            "Treble": 50
+        }
+    },
+    "Markbass Little Mark III": {
+        "eq": ["Low", "Mid Low", "Mid High", "High"],
+        "neutral": {
+            "Low": 50,
+            "Mid Low": 50,
+            "Mid High": 50,
+            "High": 50
+        }
+    },
+    "Darkglass Microtubes 900": {
+        "eq": ["Bass", "Low Mids", "High Mids", "Treble"],
+        "neutral": {
+            "Bass": 50,
+            "Low Mids": 50,
+            "High Mids": 50,
+            "Treble": 50
+        }
+    },
+    "Aguilar DB 751": {
+        "eq": ["Bass", "Midrange", "Treble"],
+        "neutral": {
+            "Bass": 50,
+            "Midrange": 50,
+            "Treble": 50
+        }
+    },
+    "Chillamp Beta": {
+        "eq": ["Bass", "Medium", "Treble"],
+        "neutral": {
+            "Bass": 50,
+            "Medium": 50,
+            "Treble": 50
+        }
+    }
+}
